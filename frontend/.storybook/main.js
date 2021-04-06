07e9a18d9463fd58a991f73b958d0ba4b3205423
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-04-06 16:18:37 -0400

    Add postcss addon for postcss 8 support

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,5 +1,15 @@
 module.exports = {
   stories: ['../src/stories/**/*.story.@(ts|tsx|js|jsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
 };
-
