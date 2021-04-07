41fee8779ac0218506479bfb9c5951fa10feeb78
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-04-07 15:54:41 -0400

    Add webpack config to use scss for styles in storybook

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -12,4 +12,12 @@ module.exports = {
       },
     },
   ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+    });
+
+    return config;
+  },
 };
