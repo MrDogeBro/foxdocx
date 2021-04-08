36b1d7a7dd2397e6ce142753d8c9a8ef8f782c31
MrDogeBro <MrDogeBro@users.noreply.github.com>
2021-04-08 19:42:38 -0400

    Add base style for button component

--- frontend/src/ui/Button.tsx
+++ frontend/src/ui/Button.tsx
@@ -1,7 +1,10 @@
 import React, { DetailedHTMLProps, ButtonHTMLAttributes } from 'react';
 
 const colorClassnames = {
-  primary: '',
+  primary:
+    'bg-blue-700 text-button bg-accent hover:bg-accent-hover disabled:text-accent-disabled disabled:bg-accent-hover',
+  secondary:
+    'text-button bg-primary-700 hover:bg-primary-600 disabled:text-primary-500 disabled:bg-primary-600',
 };
 
 export type ButtonProps = DetailedHTMLProps<
@@ -23,10 +26,12 @@ export const Button: React.FC<ButtonProps> = ({
   return (
     <button
       disabled={disabled || loading}
-      className={`${colorClassnames[color]}`}
+      className={`${colorClassnames[color]} font-bold flex items-center justify-center py-2 px-6 text-sm rounded-md transition duration-250 ease-in-out ${className}`}
       {...props}
     >
-      <span>{children}</span>
+      <span className={loading ? 'opacity-0' : 'flex items-center'}>
+        {children}
+      </span>
     </button>
   );
 };
